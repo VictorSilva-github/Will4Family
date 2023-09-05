8c638f9b8609c801a6b1ee77ec726ce1ef5c713e
Bernard Teles <bernardtelgon@gmail.com>
2023-09-05 16:13:51 -0300

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.destroy_all
+
+Victor = User.create!({
+  fullname: "Vitor",
+  birthday: Date.new(1990,01,01)
+  document_type:111,
+  document_number: 123456789,
+  phone:number: 123456789,
+  gender: "masculino"
+  post_code:123456789
+  address: "Rua Jerico"
+  city: "Sao Paulo"
+  country: "Brasil"
+  email:"cap@gmail.com"
+  password:"123456789"
+})
+
+Victor = User.create!({
+  fullname: "Bernard"
+  birthday: 1990/01/01
+  document_type:111
+  document_number: 123456789
+  phone:number: 123456789
+  gender: "masculino"
+  post_code:123456789
+  address: "Rua Jerico"
+  city: "Sao Paulo"
+  country: "Brasil"
+  email:"cap2@gmail.com"
+  password:"123456789"
+})
