d31ea95e6aec2f21a827d132d99b7a8bca93d6bb
victor silva <canalampmoficial2018@gmail.com>
2023-09-05 15:32:05 -0300

    update

--- app/models/receive_message.rb
+++ app/models/receive_message.rb
@@ -1,4 +1,4 @@
 class ReceiveMessage < ApplicationRecord
-  belongs_to :message
-  belongs_to :receive
+  belongs_to :message, foreign_key: 'message_id'
+  belongs_to :receive, foreign_key: 'receive_id'
 end
