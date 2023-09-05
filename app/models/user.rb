83a7baadaddc5b38c77a9a52fffe0562615d6c3a
Bernard Teles <bernardtelgon@gmail.com>
2023-09-05 14:59:58 -0300

    validates_user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,26 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  validades :encrypted_password, presence: true
+  validades :reset_password_token, presence: true
+  validades :reset_password_sent_at,
+  #validates #:custom_remember_created_at_validation
+  #validates :created_at, presence: true
+  #validates :updated_at, presence: true
+  validates :fullname, presence: true, length: { minimum: 2, maximum: 100 }
+  validates :birthday, presence: true, date: true
+
+  DOCUMENT_TYPE_OPTIONS = ['ID', 'Driver licence', 'Passaport']
+  validates :document_type, presence: true, inclusion: { in: DOCUMENT_TYPE_OPTIONS }
+
+  validates :document_number, presence: true
+  validates :phone_number, length: { minimum: 9, maximum: 15 }
+  validates :post_code, presence: true
+  validates :address, presence: true
+  validates :city, presence: true
+  validates :country, presence: true
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
--- db/schema.rb
+++ db/schema.rb
@@ -35,4 +35,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_04_204532) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
 end
