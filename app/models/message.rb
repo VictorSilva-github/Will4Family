cfa5798feb171e903b88f8510b83b571581aa8fb
Bernard Teles <bernardtelgon@gmail.com>
2023-09-05 19:36:24 -0300

    receive/message validates

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,4 +2,11 @@ class Message < ApplicationRecord
   belongs_to :user, foreign_key: 'user_id'
   has_many :receive_messages, foreign_key: 'message_id'
   has_many :receives, through: :receive_messages
+  validates :user_id, presence: true
+  validates :message_type, presence: true
+  validates :description, presence: true
+  validates :user_cheking, presence: true
+  validates :last_checking_at, presence: true
+  validates :tolerance_days, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :ultimate_date, presence: true
 end
--- app/models/receive.rb
+++ app/models/receive.rb
@@ -2,4 +2,9 @@ class Receive < ApplicationRecord
   belongs_to :user, foreign_key: 'user_id'
   has_many :receive_messages, foreign_key: 'receive_id'
   has_many :messages, through: :receive_messages
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :address, presence: true
+  validates :phone_number, presence: true, length: { minimum: 9, maximum: 15 }, format: { with: /\A(\(\d{2}\)|\d{2})[-.\s]?\d{4,5}[-.\s]?\d{4}\z/ }
+  validades :additional_info, presence: true
 end
