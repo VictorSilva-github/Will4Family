788f2058ea4f4d1e5f5d40556bde2bb3e2527f57
Bernard Teles <bernardtelgon@gmail.com>
2023-09-07 16:05:54 -0300

    join_table

--- app/controllers/receives_messages_controller.rb
+++
@@ -1,2 +0,0 @@
-class ReceivesMessagesController < ApplicationController
-end
--- app/models/message.rb
+++ app/models/message.rb
@@ -9,7 +9,7 @@ class Message < ApplicationRecord
   enum message_type: { last_wishes: 0, last_words: 1, passwords: 2, secrets: 3, will: 4, message: 5 }
   validates :description, presence: true
   validates :user_cheking, presence: true
-  validates :last_checking_at, presence: true
+  #validates :last_checking_at, presence: true
   validates :tolerance_days, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :ultimate_date, presence: true
 end
