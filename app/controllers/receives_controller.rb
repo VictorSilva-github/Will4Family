5d137a1b23c6dbc65649c278a9266532a8afa84e
victor silva <canalampmoficial2018@gmail.com>
2023-09-18 17:18:28 -0300

    smallTests

--- app/controllers/receives_controller.rb
+++ app/controllers/receives_controller.rb
@@ -1,68 +1,68 @@
-class ReceivesController < ApplicationController
+# class ReceivesController < ApplicationController
 
 
-  def index
-    @receives = Receive.all
-  end
+#   def index
+#     @receives = Receive.all
+#   end
 
-  def show
-    @receive = Receive.find(params[:id])
-  end
+#   def show
+#     @receive = Receive.find(params[:id])
+#   end
 
-  def new
-    if params.key?(:message)
-      @message = Message.find(params[:message])
-    end
-    @receive = Receive.new
-  end
+#   def new
+#     if params.key?(:message)
+#       @message = Message.find(params[:message])
+#     end
+#     @receive = Receive.new
+#   end
 
-  def create
-    @receive = Receive.new(receive_params)
-    @receive.user = current_user
-    if @receive.save
-      # raise
-      redirect_to new_message_receive_message_path(message_permitted[:message_id])
-    else
-      @message = Message.find(message_permitted[:message_id])
-      render 'new', status: :unprocessable_entity
-    end
-  end
+#   def create
+#     @receive = Receive.new(receive_params)
+#     @receive.user = current_user
+#     if @receive.save
+#       # raise
+#       redirect_to new_message_receive_message_path(message_permitted[:message_id])
+#     else
+#       @message = Message.find(message_permitted[:message_id])
+#       render 'new', status: :unprocessable_entity
+#     end
+#   end
 
-  def edit
-    if params.key?(:message)
-      @message = Message.find(params[:message])
-    end
-    @receive = Receive.find(params[:id])
-  end
+#   def edit
+#     if params.key?(:message)
+#       @message = Message.find(params[:message])
+#     end
+#     @receive = Receive.find(params[:id])
+#   end
 
-  def update
-    @receive = Receive.find(params[:id])
-    if @receive.update(receive_params)
-      redirect_to message_path(message_permitted[:message_id])
-    else
-      @message = Message.find(message_permitted[:message_id])
-      render 'edit', status: :unprocessable_entity
-    end
-  end
+#   def update
+#     @receive = Receive.find(params[:id])
+#     if @receive.update(receive_params)
+#       redirect_to message_path(message_permitted[:message_id])
+#     else
+#       @message = Message.find(message_permitted[:message_id])
+#       render 'edit', status: :unprocessable_entity
+#     end
+#   end
 
-  def destroy
-    @receive = Receive.find(params[:id])
-    @receive.receive_messages.destroy.all
-    @receive.destroy
-    redirect_to message_path
-  end
+#   def destroy
+#     @receive = Receive.find(params[:id])
+#     @receive.receive_messages.destroy.all
+#     @receive.destroy
+#     redirect_to message_path
+#   end
 
-  private
+#   private
 
-  def message_permitted
-    params.require(:receive).permit(:message_id)
-  end
+#   def message_permitted
+#     params.require(:receive).permit(:message_id)
+#   end
 
-  def receive_params
-    params.require(:receive).permit(:name, :email, :relationship, :phone_number, :additional_info)
-  end
+#   def receive_params
+#     params.require(:receive).permit(:name, :email, :relationship, :phone_number, :additional_info)
+#   end
 
-  def message_params
-    params.require(:message).permit(:message_type, :description, :checkin_days, :last_checking_at, :tolerance_days, :ultimate_date)
-  end
-end
+#   def message_params
+#     params.require(:message).permit(:message_type, :description, :checkin_days, :last_checking_at, :tolerance_days, :ultimate_date)
+#   end
+# end
